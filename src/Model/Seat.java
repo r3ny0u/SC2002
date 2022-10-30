50a5385f9a0d62b4893465f3e4b24cc40677d9c0
r3ny0u <renyou14@gmail.com>
2022-10-30 18:37:08 +0800

    added some for seats

--- src/Model/Seat.java
+++ src/Model/Seat.java
@@ -1,4 +1,26 @@
 package Model;
 public class Seat {
+    public String seatID;
+    public boolean assigned;
+    public String customerID;
 
+    public Seat(String seatID)
+    {
+        this.seatID = seatID;
+    }
+
+    public void assign(String customerID)
+    {
+        if (assigned == true)
+        {
+            System.out.println("Seat is already taken!");
+            return;
+        }
+        else
+        {
+            this.customerID = customerID;
+            this.assigned = true;
+        }
+    }
+    
 }

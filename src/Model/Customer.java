83c79a511875849e52bfd9edd1fa16039e68f4da
r3ny0u <renyou14@gmail.com>
2022-10-30 15:10:03 +0800

    customer interface

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -1,7 +1,10 @@
 package Model;
 import java.util.Scanner;
 
-public class Customer extends Account{
+import Database.CineplexDB;
+
+public class Customer {
+    private String customerID;
     private String name;
     private String email;
     private String mobile;
@@ -11,15 +14,14 @@ public class Customer extends Account{
     public Customer() {
     }
 
-    public Customer(String username, String password, String accountID) {
-        this.username = username;
-        this.password = password;
-        this.accountID = accountID;
-        // this.name = name;
-        // this.email = email;
-        // this.mobile = mobile;
-        // this.bookings = bookings;
-        // this.movieTickets = movieTickets;
+    public Customer(String customerID, String name, String email, String mobile, Booking[] bookings,
+            MovieTicket[] movieTickets) {
+        this.customerID = customerID;
+        this.name = name;
+        this.email = email;
+        this.mobile = mobile;
+        this.bookings = bookings;
+        this.movieTickets = movieTickets;
     }
 
     public void customerInit() {
@@ -37,27 +39,28 @@ public class Customer extends Account{
         this.mobile = sc.nextLine();
 
         System.out.printf("Please enter your customerID: ");
-        this.username = sc.nextLine();
+        this.customerID = sc.nextLine();
 
         System.out.printf("Hello %s! Welcome to MOBLIMA, the world most advanced cinema booking system.\n",
                 this.name);
     }
 
     public void printCustomerDetails() {
-        // System.out.println("\nHere are your details UwU\n");
+        System.out.println("\nHere are your details UwU\n");
 
-        // System.out.println("Name: " + this.name);
-        // System.out.println("Email: " + this.email);
-        // System.out.println("Mobile: " + this.mobile);
-        
-        System.out.println("Username: " + this.username);
-        System.out.println("Password: " + this.password);
-        System.out.println("AccountID: " + this.accountID);
+        System.out.println("Name: " + this.name);
+        System.out.println("Email: " + this.email);
+        System.out.println("Mobile: " + this.mobile);
+        System.out.println("ID: " + this.customerID);
 
-        // System.out.println("\n");
+        System.out.println("\n");
     }
 
     public void queryPurpose() {
+        MovieDB movies = new MovieDB();
+        String movieChoice, cinemaChoice;
+        int prevChoice;
+
         System.out.println("What would you like to do today?");
         System.out.println("1. View movie listings");
         System.out.println("2. View movie details");
@@ -71,19 +74,60 @@ public class Customer extends Account{
         switch (choice) {
             case 1:
                 // show movie listings
+                // read from db
+                movies.getMovies();
                 break;
             case 2:
                 // show movie details
+                // read from db
+                if (prevChoice != 1)
+                    movies.getMovies();
+                System.out.println("Please choose the movie");
+                movieChoice = scanner.next();
+                //find movie from movies[]
+                    //movie = movies[101]
+                //print details
                 break;
             case 3:
                 // display movie list
+                if (prevChoice != 1)
+                    movies.getMovies();
                 // let customer choose a movie, then display seat availability
+                System.out.println("Please choose the movie");
+                movieChoice = scanner.next();
+                //find movie from movies[]
+                System.out.println("Please choose the cineplex");
+                cineplexChoice = scanner.next();
+                System.out.println("Please choose the cinema");
+                cinemaChoice = scanner.next();
+                // use db to find cineplex
+                Cinema cinema = cineplex.findCinema(cinemaChoice);
+                //helpppp so this is like go into cinema then movie then seat?
+                cinema.findMovie(movieChoice).printSeats();
+
                 break;
             case 4:
                 // make a booking
+                // display movie list
+                movies.getMovies();
+                // let customer select their movie
+                System.out.println("Which movie do you want to book for?");
+                movieChoice = scanner.next();
+                // let customer select from the available cinemas
+                System.out.println("Please choose the cineplex");
+                cineplexChoice = scanner.next();
+                System.out.println("Please choose the cinema");
+                cinemaChoice = scanner.next();
+                // access that cinema's database
+                // print available seats
+                // let customer select their seats
+                // show confirmation page
+                // update cinema's database with new seat matrix
+
                 break;
             case 5:
                 // check booking history
+                System.out.println("Your bookings:" + this.bookings);
                 break;
             case 6:
                 // show top 5 based on ticket sales

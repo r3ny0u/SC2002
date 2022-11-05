5cb55ffde0e44bc4823c05e8fb9e3322a190ef9e
Dayna Chia <day.chia@gmail.com>
2022-11-05 12:51:35 +0800

    edited the cinema class, movie to be edited accordingly

--- src/Model/Movie.java
+++ src/Model/Movie.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Movie{
+public class Movie {
     protected String title;
     protected String status;
     protected String synopsis;
@@ -13,9 +13,11 @@ public class Movie{
     protected double overallRating;
     protected int ratingCount;
     protected ArrayList<Rating> reviews;
-    protected Map<String, Seat[]> seats;
+    protected Map<String, Seat[]> seats; // d: to be removed
     protected int salesCount;
     protected Map<String, ArrayList<String>> showingPlaces;
+    // d: idk what to call this variable, can rename
+    protected Map<String, Map<String, Seat[]>> cinemaName; // cinemaID->showtimes->seats
 
     // DO NOT MODIFY THIS CONSTRUCTOR, MAKE ANOTHER IF YOU NEED ANOTHER CONSTRUCTOR
     public Movie(String title, String status, String synopsis, String director, ArrayList<String> casts) {
@@ -26,7 +28,8 @@ public class Movie{
         this.casts = casts;
     }
 
-    public Movie(String title, String status, String synopsis, String director, ArrayList<String> casts, ArrayList<String> cinemaNames) {
+    public Movie(String title, String status, String synopsis, String director, ArrayList<String> casts,
+            ArrayList<String> cinemaNames) {
         this.title = title;
         this.status = status;
         this.synopsis = synopsis;
@@ -36,20 +39,18 @@ public class Movie{
         this.reviews = new ArrayList<Rating>();
         this.ratingCount = 0;
         this.salesCount = 0;
-        
-        int n=0,i=0,j=0;
+
+        int n = 0, i = 0, j = 0;
         Seat[] s = new Seat[100];
-        for(n=0; n < cinemaNames.size(); n++)
-        {
-            for(j=1; j<=100; j++)
-            {   
-                if(j%10 == 0)
+        for (n = 0; n < cinemaNames.size(); n++) {
+            for (j = 1; j <= 100; j++) {
+                if (j % 10 == 0)
                     i++;
-                String row = "" + (char)(65+i);
-                s[j-1] = new Seat(row + (j-1));
-                
+                String row = "" + (char) (65 + i);
+                s[j - 1] = new Seat(row + (j - 1));
+
             }
-            seats.put(cinemaNames.get(n),s);
+            seats.put(cinemaNames.get(n), s);
         }
     }
 
@@ -57,7 +58,7 @@ public class Movie{
         return this.title;
     }
 
-    public int getSalesCount(){
+    public int getSalesCount() {
         return this.salesCount;
     }
 
@@ -69,40 +70,37 @@ public class Movie{
         System.out.println("Cast      : " + String.join(", ", casts));
         System.out.println("Ratings   : " + overallRating + " / 5.0 stars");
         // for (Rating rating : reviews) {
-        //     // Haven't made this method thing yet
-        //     // rating.printRating()
+        // // Haven't made this method thing yet
+        // // rating.printRating()
         // }
     }
 
     public void printSeats(String cinemaName) {
-        int i,j;
+        int i, j;
         int[][] seatMatrix = new int[10][10];
         i = j = 0;
 
         for (Seat s : seats.get(cinemaName)) {
-            if(s.assigned)
+            if (s.assigned)
                 seatMatrix[i][j] = 1;
             else
                 seatMatrix[i][i] = 0;
             j++;
 
-            if(j == 9)
-            {
-                j=0;
+            if (j == 9) {
+                j = 0;
                 i++;
             }
             System.out.println("\n");
         }
 
         System.out.println("=================Screen=================\n");
-        for(i = 0; i < 10; i++)
-        {
-            System.out.println((char)(65+i));
-            for(j = 0; j < 10; j++)
-            {
+        for (i = 0; i < 10; i++) {
+            System.out.println((char) (65 + i));
+            for (j = 0; j < 10; j++) {
                 if (seatMatrix[i][j] == 1)
                     System.out.println("|x| ");
-                else    
+                else
                     System.out.println("|O| ");
             }
             System.out.println();
@@ -111,54 +109,50 @@ public class Movie{
         System.out.println("Legend\n|x| = taken\n|O| = available\n");
     }
 
-    public boolean assignSeat(String cinemaName, String seatID, String customerID)
-    {
+    public boolean assignSeat(String cinemaName, String seatID, String customerID) {
         Seat[] s = seats.get(cinemaName);
         int row = seatID.charAt(0);
         row -= 65;
         int col = Integer.parseInt(String.valueOf(seatID.charAt(1)));
         this.salesCount++;
-        return s[(row*10)+col].assign(customerID);
+        return s[(row * 10) + col].assign(customerID);
     }
-    
-    public boolean checkSeat(String cinemaName, String seatID)
-    {
+
+    public boolean checkSeat(String cinemaName, String seatID) {
         Seat[] s = seats.get(cinemaName);
         int row = seatID.charAt(0);
         row -= 65;
         int col = Integer.parseInt(String.valueOf(seatID.charAt(1)));
-        return s[(row*10)+col].assigned;
+        return s[(row * 10) + col].assigned;
     }
 
     public void addReviews(String customerID, String review, float rating) {
-        Rating newRating = new Model.Rating(customerID,review,rating);
+        Rating newRating = new Model.Rating(customerID, review, rating);
         reviews.add(newRating);
         this.ratingCount++;
         overallRating = ((overallRating * review.length()) + rating) /
-        (review.length() + 1);
+                (review.length() + 1);
     }
-    public double getRating(){
-        if (ratingCount>1)
+
+    public double getRating() {
+        if (ratingCount > 1)
             return overallRating;
         else
             return -1;
     }
 
     public void addShowingPlaces(String cineplexID, String cinemaID) {
-        if(showingPlaces == null) {
+        if (showingPlaces == null) {
             showingPlaces = new HashMap<>();
-                ArrayList<String> temp = new ArrayList<String>();
-                temp.add(cinemaID);
-                showingPlaces.put(cineplexID, temp);
-            
+            ArrayList<String> temp = new ArrayList<String>();
+            temp.add(cinemaID);
+            showingPlaces.put(cineplexID, temp);
+
         }
 
-
-        else if(showingPlaces.containsKey(cineplexID)) {
+        else if (showingPlaces.containsKey(cineplexID)) {
             showingPlaces.computeIfAbsent(cineplexID, k -> new ArrayList<String>()).add(cinemaID);
-        }
-        else
-        {
+        } else {
             ArrayList<String> temp = new ArrayList<String>();
             temp.add(cinemaID);
             showingPlaces.put(cineplexID, temp);
@@ -167,7 +161,8 @@ public class Movie{
 
     public void printShowingPlaces() {
         for (String key : showingPlaces.keySet()) {
-            System.out.println("\n=====================================================================\nCineplex: " + key);
+            System.out.println(
+                    "\n=====================================================================\nCineplex: " + key);
             System.out.print("Cinemas: ");
             for (String str : showingPlaces.get(key)) {
                 System.out.print(str + "    ");
